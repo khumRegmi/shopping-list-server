37ff24fe1e24637f08b00a50c760c668f6a9da20
Khum Regmi <kpr.regmi088@gmail.com>
2021-10-17 12:41:56 -0400

    fixing server file

--- server.js
+++ server.js
@@ -9,37 +9,18 @@ const app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+//Use Routes
+app.use("/api/items", items);
+
 // DB config
 const db = require("./config/keys").mongoURI;
+
+const port = process.env.PORT || 5000;
 
 //Connect to Mongo
 mongoose
   .connect(db)
-  .then(() => console.log("MongoDB Connected..."))
+  .then(() =>
+    app.listen(port, () => console.log(`Server started on port ${port}`))
+  )
   .catch((err) => console.log(err));
-
-//use Routes
-app.use("api/items", items);
-
-//connect to Mongo
-mongoose
-  .connect(db)
-  .then(() => console.log("MongoDB connected"))
-  .catch((err) => console.log(err));
-
-//Use Routes
-app.use("/api/items", items);
-
-//Serve static assets if in production
-if (process.env.NODE_ENV === "production") {
-  //Set static folder
-  app.use(express.static("client/build"));
-
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-  });
-}
-
-const port = process.env.PORT || 5000;
-
-app.listen(port, () => console.log(`Server started on port ${port}`));
